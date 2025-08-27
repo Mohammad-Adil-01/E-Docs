1d104f779d5d4d9a297921df7681deaa7fa06248
Mohammad Adil Shaikh <165527549+Mohammad-Adil-01@users.noreply.github.com>
2025-08-28 00:47:31 +0530

    Delete Next-js directory

--- Next-js/my-nextjs-app/src/app/home/page.js
+++
@@ -1,5 +0,0 @@
-export default function home(){
-    return <>
-        <h1>Hello Welcome to Home PAGE</h1>
-    </>
-}
--- Next-js/my-nextjs-app/src/components/menu/menu.jsx
+++
@@ -1,5 +0,0 @@
-export default function menu (){
-    return <>
-    <h1>Hello menu</h1>
-    </>
-}
